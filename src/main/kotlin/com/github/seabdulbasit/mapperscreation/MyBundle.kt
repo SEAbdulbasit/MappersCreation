821e0383df4c8f905cd8a31464883e302354b8a4
GitHub Action <action@github.com>
2022-01-15 17:32:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/seabdulbasit/mapperscreation/MyBundle.kt
+++ src/main/kotlin/com/github/seabdulbasit/mapperscreation/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.seabdulbasit.mapperscreation
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/seabdulbasit/mapperscreation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.seabdulbasit.mapperscreation.services
+
+import com.github.seabdulbasit.mapperscreation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
